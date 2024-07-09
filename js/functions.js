fe5e697909550ae47f301b3aefb981673da46529
Абдуллазим <abdullazimimarov497@gmail.com>
2024-07-09 22:17:59 +0300

    task

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+/* eslint-disable*/
+const stringCheck = function (string, maxLenth) {
+  let check = (string.length <= maxLenth) ? true : false;
+  console.log(check)
+}
+
+stringCheck('skldlkasfdgfdsvgdfslkfldkglkfd,/.,,ll', 100)
+
+
+
