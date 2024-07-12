e014e7e4c821e3aa86b10b22a2f6f43ddb99306d
abdullazim <abdullazimimarov497@gmail.com>
2024-07-12 12:35:09 +0300

    module2-task1

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,27 @@
-/* eslint-disable*/
-const stringCheck = function (string, maxLenth) {
+
+const stringCheck = (string, maxLenth) => {
   let check = (string.length <= maxLenth) ? true : false;
   console.log(check)
 }
-
-stringCheck('skldlkasfdgfdsvgdfslkfldkglkfd,/.,,ll', 100)
-
+stringCheck('skldlkasfdgfdsvgdfslkfldkglkf ,/.,,ll', 100)
 
 
+
+const getPalindrome = (input) => {
+  const normalizedStr = input.replaceAll(' ', '').toLowerCase();
+
+  let reversedStroke = '';
+
+  for (let i = normalizedStr.length - 1; i >= 0; --i) {
+    reversedStroke += normalizedStr[i];
+  }
+
+  console.log(reversedStroke);
+
+  return normalizedStr === reversedStroke;
+};
+
+console.log(getPalindrome('топот'));
+console.log(getPalindrome('ДовОд'));
+console.log(getPalindrome('Кекс'));
+console.log(getPalindrome('Лёша на полке клопа нашёл'));
