ad321a41f8d2172dc0bfbafdb98776c292121141
abdullazim <158089786+kkurut@users.noreply.github.com>
2024-07-25 15:15:23 +0300

    Module8-task2

--- js/display-comments.js
+++ js/display-comments.js
@@ -1,27 +1,44 @@
 const socialCommentsElement = document.querySelector('.social__comments');
+const commentsLoaderElement = document.querySelector('.comments-loader');
+const socialCommentElement = document.querySelector('.social__comment');
 
-//* создание коментария
+const NEXT_NUMBERS_QTY = 5;
+
+let currentComments = [];
+let startIndex = 0;
+
 const createComment = ({ avatar, name, message }) => {
-  const socialCommentElement = document.querySelector('.social__comment').cloneNode(true);
-  const socialPicture = socialCommentElement.querySelector('.social__picture');
+  const clonedSocialCommentElement = socialCommentElement.cloneNode(true);
+  const socialPicture = clonedSocialCommentElement.querySelector('.social__picture');
 
   socialPicture.src = avatar;
   socialPicture.alt = name;
-  socialCommentElement.querySelector('.social__text').textContent = message;
-  return socialCommentElement;
+  clonedSocialCommentElement.querySelector('.social__text').textContent = message;
+  return clonedSocialCommentElement;
 };
 
-//* создание списка коментариев текущей мииатюры
-const displayComments = (comments) => {
+const displayMoreComments = () => {
   const fragment = document.createDocumentFragment();
+  const endIndex = Math.min(startIndex + NEXT_NUMBERS_QTY, currentComments.length);
 
-  comments.forEach((comment) => {
-    const commentElement = createComment(comment);
-    fragment.appendChild(commentElement);
+  currentComments.slice(startIndex, endIndex).forEach((comment) => {
+    fragment.appendChild(createComment(comment));
   });
 
-  socialCommentsElement.innerHTML = '';
   socialCommentsElement.appendChild(fragment);
+  startIndex += NEXT_NUMBERS_QTY;
+
+  if (startIndex >= currentComments.length) {
+    commentsLoaderElement.classList.add('hidden');
+  }
 };
 
-export { displayComments };
+const displayComments = (comments) => {
+  startIndex = 0;
+  currentComments = comments;
+  socialCommentsElement.innerHTML = '';
+  commentsLoaderElement.classList.remove('hidden');
+  displayMoreComments();
+};
+
+export { displayComments, displayMoreComments, commentsLoaderElement };
