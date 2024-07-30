4ced239950983b79d622278f0eefb31f3da578d4
abdullazim <abdullazimimarov497@gmail.com>
2024-07-30 15:48:43 +0300

    module11-task1

--- js/get-data.js
+++ js/get-data.js
@@ -1,24 +1,30 @@
 const bodyElement = document.querySelector('body');
 const errorTemplateElement = document.querySelector('#data-error').content.querySelector('.data-error');
 
-export async function fetchData() {
-  try {
-    const response = await fetch('https://32.jaavascript.htmlacademy.pro/kekstagram/data'); // Замените URL на ваш
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    const data = await response.json(); // Преобразуем ответ в формат JSON
-    return data; // Возвращаем данные
-  } catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-    const errorCreate = () => {
-      const errorElement = errorTemplateElement.cloneNode(true);
-      bodyElement.append(errorElement);
-      setTimeout(() => {
-        errorElement.remove()
-      }, 5000)
-    }
-    errorCreate();
-    throw error;
-  }
-}
+const promise = new Promise((resolve, reject) => {
+  return fetch('https://32.javascript.htmlacademy.pro/kekstagram/data')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data)
+      resolve(data)
+    })
+    .catch((error) => {
+      console.error('There was a problem with the fetch operation:', error);
+      const errorCreate = () => {
+        const errorElement = errorTemplateElement.cloneNode(true);
+        bodyElement.append(errorElement);
+        setTimeout(() => {
+          errorElement.remove()
+        }, 5000)
+      }
+      errorCreate()
+      reject(error)
+    });
+});
+
+export { promise };
--- js/main.js
+++ js/main.js
@@ -1,24 +1,12 @@
-// main.js
-import { fetchData } from './get-data.js';
+import {promise } from './get-data.js';
 import { similarPictures } from './render-thumbnail.js';
 import { renderBigPicture } from './big-picture.js';
 import { openAndCloseForm } from './form.js';
 import { getEditImg } from './edit-picture.js';
 
-// Асинхронная функция для инициализации приложения
-async function init() {
-    try {
-        const data = await fetchData(); // Получаем данные с сервера
-
-        similarPictures(data); // Передаем данные в similarPictures
-        renderBigPicture(data); // Передаем данные в renderBigPicture
-    } catch (error) {
-        console.error('Error initializing application:', error);
-    }
-
-    openAndCloseForm();
-    getEditImg();
-}
-
-// Запускаем инициализацию приложения
-init();
+promise.then((data) => {
+  similarPictures(data);
+  renderBigPicture(data);
+  openAndCloseForm();
+  getEditImg();
+});
